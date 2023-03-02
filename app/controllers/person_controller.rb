a12ae51da31b7cf6d4bf34398bb0668677c8e5a3
jthierce <jordan.thiercelin@gmail.com>
2023-03-02 03:42:07 +0100

    beginning of the import

--- app/controllers/person_controller.rb
+++ app/controllers/person_controller.rb
@@ -1,6 +1,8 @@
 class PersonController < ApplicationController
     def import
         file = params[:file]
-        # later return/notice some error in the view mode.
+        # add some errors in the view mode if the file is not csv or valid
+        Person.import(file)
+        # add notice when the import is suceed.
     end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,16 @@
 class Person < ApplicationRecord
-    high_level_attribute :email, :mobile_phone_number, :home_phone_number, :address
+  high_level_attribute :email, :mobile_phone_number, :home_phone_number, :address
 
-    #before_save
+  def self.import file
+    csv = CSV.parse(file, headers: true)
+    csv.each do |row|
+      person_hash = {}
+      person_hash[:reference] = row[:reference]
+      person_hash[:firstname] = row[:firstname]
+      person_hash[:lastname] = row[:lastname]
+      person_hash[:home_phone_number] = row[:home_phone_number]
+      person_hash[:mobile_phone_number] = row[:mobile_phone_number]
+      person_hash[:email] = row[:email]
+      person_hash[:address] = row[:address]
+  end
 end
